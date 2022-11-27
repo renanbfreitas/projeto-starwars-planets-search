13e65f52c15c307b4b84b349501e4c4d653b7bca
Renanbfreitas <renanbf1992@outlook.com>
2022-11-27 14:02:35 -0300

    Adicionando item 5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import mockList from './mockList.test';
 import App from '../App';
 
-test.skip('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('1- Testando o arquivo App', () => {
+  test('1- Verifica se a API está sendo renderizada.', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({ json: jest.fn().mockResolvedValue(mockList) });
+    render(<App />);
+    const alderaan = await screen.findByText(/alderaan/i);
+    expect(alderaan).toBeVisible();
+  });
+
+  test('2- Verifica se os botões de Filtro e Remover Filtros estão presentes na paǵina', ()=>{
+    render(<App />)
+    const buttonFilter = screen.getByRole('button', { name : /Filtro/i})
+    expect(buttonFilter).toBeInTheDocument()
+    const buttonRmvFilter = screen.getByRole('button', { name : /Remover Filtros/i})
+    expect(buttonRmvFilter).toBeInTheDocument()
+  })
+
+  test('3- Verifica se o botão Filtro está habilitado', () => {
+    render(<App />)
+    const buttonFilter = screen.getByTestId('button-filter');
+    userEvent.click(buttonFilter);
+    const operatorFilter = screen.getByText('maior que')
+    expect(operatorFilter).toHaveTextContent('maior que')
+  })
+  });
